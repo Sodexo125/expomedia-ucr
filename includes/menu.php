e5543eb8aa43b284832a66f19d7cdb1c7bc14598
Sodexo125 <leandrobelloucr@gmail.com>
2018-07-20 05:53:53 -0600

    f
    f

--- includes/menu.php
+++ includes/menu.php
@@ -1,29 +1,33 @@
-<nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top align-middle">
-	<div class="favIcon"><a href="#"><img src="./assets/images/logo-itm.png" alt="Logo de Informática y Tecologia Multimedia" ></a></div>
-    <!-- <img src=“image.jpg” alt=“image description” title=“image tooltip”> -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse nav-rock" id="navbarNavDropdown">
-    <ul class="navbar-nav ml-auto top-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Videos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Torneos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="stand.html">Stands de importancia</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          About
-        </a>
-        <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Conferencistas y Capacitadores</a>
-          <a class="dropdown-item" href="#">ExpoMedia</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+<div class="container-fluid">
+<div class="row">
+  <nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top align-middle">
+      <div class="favIcon"><a href="#"><img src="./assets/images/logo-itm.png" alt="Logo de Informática y Tecologia Multimedia" ></a></div>
+        <!-- <img src=“image.jpg” alt=“image description” title=“image tooltip”> -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse nav-rock" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto top-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Videos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Torneos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="stand.html">Stands de importancia</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              About
+            </a>
+            <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Conferencistas y Capacitadores</a>
+              <a class="dropdown-item" href="#">ExpoMedia</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</div>
+</div>
