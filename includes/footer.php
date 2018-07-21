700c2f82302fa97c3f729a91eb92a9936a638d23
judasamok <39342882+judasamok@users.noreply.github.com>
2018-07-21 15:11:29 -0600

    Ya tiene contenido

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<div class="vFooter">
+  <div class="container">
+    <h3>Sigue la expomedia</h3>
+    <div class="img-footer container-fluid"></div>
+    <div class="menu-foot container-fluid">
+      <ul class="list">
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </div>
+  </div>  
+</div><!--Fin del footer-->
