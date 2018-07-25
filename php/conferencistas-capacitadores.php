acf0f631d91275c68beda71334035cfd6ced9c04
judasamok <39342882+judasamok@users.noreply.github.com>
2018-07-25 01:46:21 -0600

    Add files via upload

--- php/conferencistas-capacitadores.php
+++ php/conferencistas-capacitadores.php
@@ -31,7 +31,7 @@
   </div>
 
   <!--Section de los invitados con imagenes-->
-  <section>
+  <section class="newSection">
     <div class="container-fluid">
       <!--Primera linea-->
       <div class="row">
@@ -118,7 +118,7 @@
   </section>
 
   <!--Section con la información de los invitados-->
-	<section>
+	<section class="newSection">
     <div class="row">
       <div class="col-md-6 marginCol6">
         <div id= "carouselExampleSlidesOnly" class= "carousel  contenedor-Slide" data-interval= "0">
@@ -161,78 +161,78 @@
       <div class="col-md-6">
         <div class="contenedor-Txt">
           <div id="texto1">
-            <h2>John Wayne - Conferencista</h2>
+            <h2 class="newH2">John Wayne - Conferencista</h2>
             <p>
               Diseñador Creativo, Fotógrafo Profesional y Productor Audiovisual costarricense/estadounidense. Ha incursionado en el área de la fotografía por más de 20 años y ha sido un fotógrafo independiente desde sus inicios.<br>
               En el año 2012 formó su propia academia JWA.CRC ®, en donde transmite de manera independiente sus conocimientos como fotógrafo profesional. Además, fue invitado especial para impartir clases y compartir sus conocimientos en universidades tanto estatales como privados en todo el país.
             </p>
-            <a href="https://universidades.cr/carreras/fotografia">Más información sobre el conferencista</a>
+            <a class="newA" href="https://universidades.cr/carreras/fotografia">Más información sobre el conferencista</a>
           </div>
           <div id="texto2" style="display:none;">
-            <h2>Clint Eastwood - Conferencista</h2>
+            <h2 class="newH2">Clint Eastwood - Conferencista</h2>
             <p>
               Es un Database Manager, Scrum Master y Big Data: Web Mining costarricense/inglés, dedicado al desarrollo de software de análisis de datos aplicados y aprendizaje estadístico automático (statistical machine learning) y a empresas para la resolución de problemas en una pymes, mediana o grande empresa.<br>
               También se centra en análisis avanzados, que incluyen análisis predictivos, análisis de redes sociales, análisis de texto y enfoques de análisis de big data. Creó su propia empresa en el 2013 y le brindado sus servicios a empresas como Corporación Pipasa, Florida Bebidas, Belcorp Costa Rica, entre otras.
             </p>
-            <a href="https://www.tec.ac.cr/fundatec/especializacion-big-data-specialist">Más información sobre el conferencista</a>
+            <a class="newA" href="https://www.tec.ac.cr/fundatec/especializacion-big-data-specialist">Más información sobre el conferencista</a>
           </div>
           <div id="texto3" style="display:none;">
-            <h2>Dayanna Jiménez - Conferencista</h2>
+            <h2 class="newH2">Dayanna Jiménez - Conferencista</h2>
             <p>
               Cocreadora de la empresa de desarrollo de videojuegos Point Break, empresa dedicada a crear simuladores con tecnología de VR (virtual reality o realidad virtual) y juegos educativos sobre el manejo adecuado de los residuos sólidos, enseñanza sobre la contaminación sónica, enfermedades transmitidas por el mosquito Aedes aegypti (dengue, zika y chikungunya), en contra del matonismo (bullying), entre otros hacia el público meta dentro del rango de edad 7 a 15 años.
             </p>
-            <a href="http://flowlab.io/">Más información sobre la conferencista</a>
+            <a class="newA" href="http://flowlab.io/">Más información sobre la conferencista</a>
           </div>
 
           <div id="texto4" style="display:none;">
-            <h2>Sebastian Brenes - Capacitador</h2>
+            <h2 class="newH2">Sebastian Brenes - Capacitador</h2>
             <p>
               Estudiante de la carrera de Informática y Tecnología Multimedia, influenciado por su padre (escultor) y madre (profesora de ballet) se dedicó a pulir sus habilidades en el dibujo. Tuvo la oportunidad de acompañar a su padre a hacer esculturas indígenas costarricense en Francia y fue invitado a dar un taller sobre dibujo en una Convención anual de anime japonés en Puntarenas y San José.
             </p>
-            <a href="https://www.facebook.com/sebastian.brenes.9">Más información sobre el capacitador</a>
+            <a class="newA" href="https://www.facebook.com/sebastian.brenes.9">Más información sobre el capacitador</a>
           </div>
 
           <div id="texto5" style="display:none;">
-            <h2>Enrique Soto V.- Capacitador</h2>
+            <h2 class="newH2">Enrique Soto V.- Capacitador</h2>
             <p>
               Es un Ingeniero de software costarricense, dedicado al desarrollo de planos para integrarlos en arduinos y raspberry pi. Comenzó como un hobby y poco a poco el conocimiento fue creciendo a tal punto que pasó de hablar sobre hardware libre a sus compañeros de trabajo a ser invitado a dar conferencias en eventos de la comunidad del software libre.
             </p>
-            <a href="https://www.hwlibre.com/">Más información sobre el capacitador</a>
+            <a class="newA" href="https://www.hwlibre.com/">Más información sobre el capacitador</a>
           </div>
 
           <div id="texto6" style="display:none;">
-            <h2>Andy Vásquez - Capacitador</h2>
+            <h2 class="newH2">Andy Vásquez - Capacitador</h2>
             <p>
               Es un estudiante de Informática Empresarial de la Universidad de Costa Rica que se interesó como hobbie en trabajar con los arduinos porque al ser una plataforma de código abierto (open-source) basada en hardware y software podía crear prototipos flexibles y fáciles de usar para crear objetos o entornos interactivos.<br>
               Por el momento, no está trabajando en una empresa pero su nombre es conocido en la comunidad de Software Libre.
             </p>
-            <a href="https://www.facebook.com/groups/arduinocr/about/">Más información sobre el capacitador</a>
+            <a class="newA" href="https://www.facebook.com/groups/arduinocr/about/">Más información sobre el capacitador</a>
           </div>
 
           <div id="texto7" style="display:none;">
-            <h2>Lucas Mora C. - Capacitador</h2>
+            <h2 class="newH2">Lucas Mora C. - Capacitador</h2>
             <p>
               Es un Máster Profesional en Computación e Informática y es egresado de la carrera de Computación con énfasis en Ingeniería de software de la Universidad de Costa Rica. Trabajó para Twitter en la parte del mantenimiento de la aplicación, Google y eventualmente creó una empresa con varios colegas de su anterior lugar de trabajo.<br>
               Se dedica a la creación de aplicativos para celular para empresas privadas, ha trabajado para instituciones públicas como asesor y en algunas ocasiones es profesor en varias universidades privadas de Costa Rica.
             </p>
-            <a href="https://www.xamarin.com">Más información sobre el capacitador</a>
+            <a class="newA" href="https://www.xamarin.com">Más información sobre el capacitador</a>
           </div>
 
           <div id="texto8" style="display:none;">
-            <h2>Josefina Torres - Capacitadora</h2>
+            <h2 class="newH2">Josefina Torres - Capacitadora</h2>
             <p>
               Estudió en la Facultad de Bellas Artes como Diseñadora gráfica y trabajó para varias maquilas haciendo diseños para terceros. Eventualmente observó el potencial de las aplicaciones y estudió en varios cursos online de programación para acreditarse con el objetivo final de crear su propia empresa, Solar Baboon, de desarrollo de aplicaciones.<br>
               Al pasar de los años, la empresa también trabaja en el desarrollo de aplicaciones híbridas por las ventajas que a los usuarios y por su bajo costo de producción.
             </p>
-            <a href="https://aplicacionesmovil.com/tecnologia/desarrollo-de-aplicaciones-hibridas/">Más información sobre la capacitadora</a>
+            <a class="newA" href="https://aplicacionesmovil.com/tecnologia/desarrollo-de-aplicaciones-hibridas/">Más información sobre la capacitadora</a>
           </div>
 
           <div id="texto9" style="display:none;">
-            <h2>Mónica Rodríguez S. - Capacitadora</h2>
+            <h2 class="newH2">Mónica Rodríguez S. - Capacitadora</h2>
             <p>
               Programadora egresada de la carrera de Ingeniería en Computación del Tecnológico de Costa Rica (TEC). Se dedica al frontend y backend del desarrollo de páginas web en la empresa Cisne Rojo ubicada en Alajuela. La empresa se dedica a la venta de servicios a instituciones estatales como la Municipalidad de Escazú, Instituto Nacional de la Mujer, Municipalidad de San Carlos, entre otros.
             </p>
-            <a href="https://developers.google.com/web/progressive-web-apps/">Más información sobre la capacitadora</a>
+            <a class="newA" href="https://developers.google.com/web/progressive-web-apps/">Más información sobre la capacitadora</a>
           </div>
         </div><!--fin contenedor-Txt-->
       </div><!--fin del col-6-->
