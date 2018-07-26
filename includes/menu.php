d189dabe7fb2855a7ca1ae80ee0413b1fbd4cb9d
Sodexo125 <leandrobelloucr@gmail.com>
2018-07-26 00:29:34 -0600

    NAV BLUIR
    Se agrego el nav con efecto responsivo y bluir

--- includes/menu.php
+++ includes/menu.php
@@ -1,4 +1,4 @@
-<nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top align-middle">
+<!-- <nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top align-middle">
 	<div class="favIcon"><a href="#"><img src="./assets/images/logo-itm.png" alt="Logo de Informática y Tecologia Multimedia" ></a></div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -25,4 +25,51 @@
       </li>
     </ul>
   </div>
+</nav> -->
+
+
+<nav class="navbar navbar-expand-md navbar-dark  fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="./assets/images/logo-itm.png" class="img-responsive logo" alt="logo">
+        </a>
+        <button id="boton-menu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Videos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Torneos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="stand.html">Stands de importancia</a>
+                </li>
+                
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Nosotros</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Conferencistas y Capacitadores</a>
+                        <a class="dropdown-item" href="#">ExpoMedia</a>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    </div>
 </nav>
+
+
+<div id="videoITM" class="embed-responsive embed-responsive-16by9 videoHeader border-bottom">
+    <video class="embed-responsive-item" src="./assets/video/videoHeader2.mp4" autoplay loop muted></video>
+</div>
+
+
+ <div id="titulo-expomedia" class="textHeader">
+    <h1>EXPOMEDIA</h1>
+</div>
