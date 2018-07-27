e408a63049e57490ea54f9ae52b48d9bfeebdb07
Sodexo125 <leandrobelloucr@gmail.com>
2018-07-26 18:30:04 -0600

    menu2 arregldo
    Se modifico el menu 2
    Hora 6:29pm

--- includes/menu2.php
+++ includes/menu2.php
@@ -1,6 +1,5 @@
-<nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top">
+<!-- <nav class="navscroll navbar navbar-expand-lg navbar-dark fixed-top">
 	<div class="favIcon"><a href=""><img src="../assets/images/logo-itm.png" alt="Logo de Informática y Tecologia Multimedia" ></a></div>
-    <!-- <img src=“image.jpg” alt=“image description” title=“image tooltip”> -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -26,4 +25,43 @@
       </li>
     </ul>
   </div>
+</nav> -->
+
+ 
+ <nav id="mynav" class="navbar navbar-expand-md navbar-dark  fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/images/logo-itm.png" class="img-responsive logo" alt="logo">
+        </a>
+        <button id="boton-menu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="videos.php">Videos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="torneos.php">Torneos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="stands.php">Stands de importancia</a>
+                </li>
+                
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Nosotros</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="conferencistas-capacitadores.php">Conferencistas y Capacitadores</a>
+                        <a class="dropdown-item" href="expomedia.php">ExpoMedia</a>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    </div>
 </nav>
+
+
