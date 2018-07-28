44ec842fcfae55aae14fcbab2b9737304d94e6a4
Sodexo125 <leandrobelloucr@gmail.com>
2018-07-28 00:47:25 -0600

    prueba.php tema
    mas código como loco.

--- php/conferencistas-capacitadores.php
+++ php/conferencistas-capacitadores.php
@@ -27,11 +27,14 @@
       </div>
     </div>
   </header>
-
+  
+  <br>
   <div id="contenido-conferencistasCapacitadores">
 
-    <div class="espacios">
-      <h1 class="infoCenterVertical">CONFERENCISTAS Y CAPACITADORES</h1>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4 text-center">CONFERENCISTAS Y CAPACITADORES</h1>
+      </div>
     </div>
 
     <!--Section de los invitados con imagenes-->
